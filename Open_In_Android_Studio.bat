3b2291ce98ca4f6a07be5f6409793229acc52695
SAICHARAN-NAYAK <saicharannayak03@gmail.com>
2026-09-09 21:12:04 +0530

    Fix syntax in Open_In_Android_Studio.bat

--- Open_In_Android_Studio.bat
+++ Open_In_Android_Studio.bat
@@ -1,23 +1,33 @@
 @echo off
+setlocal
 title Open CYBERSHIELD in Android Studio
+
+set "PROJECT_DIR=%~dp0android_studio_app"
+set "STUDIO_EXE=C:\Program Files\Android\Android Studio\bin\studio64.exe"
+
 echo ===============================================================================
 echo  CYBERSHIELD // Opening Android Studio Project
-echo  Project Path: %~dp0android_studio_app
+echo  Project Path: %PROJECT_DIR%
 echo ===============================================================================
 echo.
 
-set STUDIO_EXE=C:\Program Files\Android\Android Studio\bin\studio64.exe
+if exist "%STUDIO_EXE%" goto :launch_studio
+goto :studio_not_found
 
-if exist "%STUDIO_EXE%" (
-    echo [OK] Found Android Studio at: "%STUDIO_EXE%"
-    echo [OK] Launching Android Studio with CYBERSHIELD HTML Project...
-    start "" "%STUDIO_EXE%" "%~dp0android_studio_app"
-    echo.
-    echo Android Studio is starting up.
-    echo Select your device or emulator and click 'Run' (Shift+F10).
-) else (
-    echo [!] Could not locate Android Studio at "%STUDIO_EXE%".
-    echo Please open Android Studio manually and select:
-    echo "%~dp0android_studio_app"
-    pause
-)
+:launch_studio
+echo [OK] Located Android Studio: "%STUDIO_EXE%"
+echo [OK] Spawning Android Studio IDE...
+start "" "%STUDIO_EXE%" "%PROJECT_DIR%"
+echo.
+echo Android Studio is launching.
+echo Once open, select your device or emulator and click Run [Shift+F10].
+goto :end
+
+:studio_not_found
+echo [!] Could not locate Android Studio at default path.
+echo Please open Android Studio manually, select 'File' -^> 'Open' and choose:
+echo "%PROJECT_DIR%"
+pause
+
+:end
+endlocal
