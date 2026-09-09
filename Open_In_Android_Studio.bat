24bc3c4dcdea792202251fa7f51aa0648ed2fb3e
SAICHARAN-NAYAK <saicharannayak03@gmail.com>
2026-09-09 22:55:02 +0530

    fix: prioritize Android Studio1 installation path and prevent premature console exit

--- Open_In_Android_Studio.bat
+++ Open_In_Android_Studio.bat
@@ -18,14 +18,15 @@ if not exist "%PROJECT_DIR%" (
     exit /b 1
 )
 
-:: Check common installation paths
+:: Check installation paths (Android Studio1 first, then Android Studio, then LocalAppData)
 set "STUDIO_EXE="
-if exist "C:\Program Files\Android\Android Studio\bin\studio64.exe" (
+if exist "C:\Program Files\Android\Android Studio1\bin\studio64.exe" (
+    set "STUDIO_EXE=C:\Program Files\Android\Android Studio1\bin\studio64.exe"
+) else if exist "C:\Program Files\Android\Android Studio\bin\studio64.exe" (
     set "STUDIO_EXE=C:\Program Files\Android\Android Studio\bin\studio64.exe"
 ) else if exist "%LOCALAPPDATA%\Programs\Android Studio\bin\studio64.exe" (
     set "STUDIO_EXE=%LOCALAPPDATA%\Programs\Android Studio\bin\studio64.exe"
 ) else (
-    :: Check JetBrains Toolbox install location
     for /f "delims=" %%I in ('dir /b /s "%LOCALAPPDATA%\JetBrains\Toolbox\apps\AndroidStudio\bin\studio64.exe" 2^>nul') do (
         set "STUDIO_EXE=%%I"
     )
@@ -34,10 +35,17 @@ if exist "C:\Program Files\Android\Android Studio\bin\studio64.exe" (
 if defined STUDIO_EXE (
     echo [OK] Located Android Studio: "!STUDIO_EXE!"
     echo [OK] Spawning Android Studio IDE...
+    echo.
+    echo Launching Android Studio. Please wait 10-15 seconds for the splash screen...
     start "" "!STUDIO_EXE!" "%PROJECT_DIR%"
     echo.
-    echo Android Studio is launching.
-    echo Once open, select your device or emulator and click Run [Shift+F10].
+    echo [SUCCESS] Android Studio process started.
+    echo Once the IDE finishes loading:
+    echo   1. Wait for Gradle sync to complete.
+    echo   2. Select your device or emulator.
+    echo   3. Click Run [Shift+F10].
+    echo.
+    pause
 ) else (
     echo [!] Could not locate Android Studio automatically.
     echo Opening project folder in File Explorer...
@@ -45,6 +53,7 @@ if defined STUDIO_EXE (
     echo.
     echo Please open Android Studio manually, select File -^> Open, and choose:
     echo "%PROJECT_DIR%"
+    echo.
     pause
 )
 
